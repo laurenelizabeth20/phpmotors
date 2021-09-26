0404abbe9a456c45b9ad09cbaa134043726fc933
laurenelizabeth20 <laurensore@gmail.com>
2021-09-25 22:26:06 -0600

    Enhancement 1

---
+++ phpmotors/home.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Home | PHP Motors</title>
+        <link rel="stylesheet" title="CSS" media="screen" href="home.css">
+    </head>
+    <body>
+        <div class="content">
+            <header>
+                <img src="images/site/logo.png" alt="PHP Motors Logo">
+                <a class="account" href="#">My Account</a>
+            </header>
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">Classic</a>
+                <a href="#">Sports</a>
+                <a href="#">SUV</a>
+                <a href="#">Trucks</a>
+                <a href="#">Used</a>
+            </nav>
+            <main>
+                <h1>Welcome to PHP Motors!</h1>
+                <div class="blurb">
+                    <h3>DMC Delorean</h3>
+                    <p>3 Cup holders</p>
+                    <p>Superman doors</p>
+                    <p>Fuzzy dice!</p>
+                    <div id="own">
+                        <button>Own Today</button>
+                    </div>
+                </div>
+                <div class="upgrade-review">
+                    <div class="reviews">
+                        <h2>DMC Delorean Reviews</h2>
+                        <ul>
+                            <li>"So fast its almost like traveling in time." (4/5)</li>
+                            <li>"Coolest ride on the road." (4/5)</li>
+                            <li>"I'm feeling like Marty McFly!" (5/5)</li>
+                            <li>"The most futuristic ride of our day." (4.5/5)</li>
+                            <li>"80's livin and I love it!" (5/5)</li>
+                        </ul>
+                    </div>
+                    <div class="upgrades">
+                        <h2>Delorean Upgrades</h2>
+                        <div class="flux">
+                            <img src="images/upgrades/flux-cap.png" alt="Flux Capacitor"><br>
+                            <a href="#">Flux Capacitor</a>
+                        </div>
+                        <div class="flame">
+                            <img src="images/upgrades/flame.jpg" alt="Flame Decals"><br>
+                            <a href="#">Flame Decals</a>
+                        </div>
+                        <div class="stickers">
+                            <img src="images/upgrades/bumper_sticker.jpg" alt="Bumper Stickers"><br>
+                            <a href="#">Bumper Stickers</a>
+                        </div>
+                        <div class="hub">
+                            <img src="images/upgrades/hub-cap.jpg" alt="Hub Cap"><br>
+                            <a href="#">Hub Cap</a>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+            </main>
+            <footer>
+                <p>&copy; PHP Motors, All rights reserved.</p>
+                <p>All images used are believed to be in "Fair Use". Please notify the author if any are not and they will be removed.</p>
+                <p>Last Updated: 30 March, 2018</p>
+            </footer>
+        </div>
+    </body>
+</html>
---
+++ phpmotors/template.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Content Title | PHP Motors</title>
+        <link rel="stylesheet" title="CSS" media="screen" href="template.css">
+    </head>
+    <body>
+        <div class="content">
+            <header>
+                <img src="images/site/logo.png" alt="PHP Motors Logo">
+                <a class="account" href="#">My Account</a>
+            </header>
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">Classic</a>
+                <a href="#">Sports</a>
+                <a href="#">SUV</a>
+                <a href="#">Trucks</a>
+                <a href="#">Used</a>
+            </nav>
+            <main>
+                <h1>Content Title Here</h1>
+                <hr>
+            </main>
+            <footer>
+                <p>&copy; PHP Motors, All rights reserved.</p>
+                <p>All images used are believed to be in "Fair Use". Please notify the author if any are not and they will be removed.</p>
+                <p>Last Updated: 30 March, 2018</p>
+            </footer>
+        </div>
+    </body>
+</html>
